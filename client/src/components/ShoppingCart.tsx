3a751d7bbc800ccb3cf3622e1c82adf7adbfd713
EvgheniiMorogoi <jekamorogai@gmail.com>
2023-10-01 21:44:02 +0300

    changed viewing in shopping cart

--- client/src/components/ShoppingCart.tsx
+++ client/src/components/ShoppingCart.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Offcanvas, Stack } from "react-bootstrap";
-import { useEffect, useState } from "react"; // Import useState and useEffect
 import { useShoppingCart } from "../context/ShoppingCartContext";
-import { CartItem } from "./CartItem";
 import { formatCurrency } from "../utilities/formatCurrency";
-import { fetchItemsFromDatabase } from "../data/api";
+import { CartItem } from "./CartItem";
+import { useEffect, useState } from "react";
+import axios from "axios"; // Import Axios
 
 type ShoppingCartProps = {
   isOpen: boolean;
@@ -12,28 +12,21 @@ type ShoppingCartProps = {
 
 export function ShoppingCart({ isOpen }: ShoppingCartProps) {
   const { closeCart, cartItems } = useShoppingCart();
-  const [fetchedCartItems, setFetchedCartItems] = useState<{
-    id: number ,
-    name: string ,
-    price: number ,
-    quantity:number,
-    imgUrl: string
-  }[]>([]);
+  const [fetchedCartItems, setFetchedCartItems] = useState<any[]>([]);
 
   useEffect(() => {
-    fetchItemsFromDatabase().then((items) => {
-      // Map the fetched items to match the expected properties
-      const mappedItems = items.map((item: { id: any; name: any; price: any; quantity: any; imgUrl: any; }) => ({
-        id: item.id,
-        name: item.name,
-        price: item.price,
-        quantity: 1, // Set the initial quantity as needed
-        imgUrl: item.imgUrl
-      }));
-      setFetchedCartItems(mappedItems);
-    });
+    async function fetchItems() {
+      try {
+        const response = await axios.get("http://localhost:5000/api/get/products"); // Use Axios for fetching data
+        const data = response.data;
+        setFetchedCartItems(data);
+      } catch (error) {
+        console.error("Error fetching items:", error);
+      }
+    }
+
+    fetchItems();
   }, []);
-  
 
   return (
     <Offcanvas show={isOpen} onHide={closeCart} placement="end">
@@ -42,21 +35,23 @@ export function ShoppingCart({ isOpen }: ShoppingCartProps) {
       </Offcanvas.Header>
       <Offcanvas.Body>
         <Stack gap={3}>
-        {cartItems.map((cartItem) => {
-          const item = fetchedCartItems.find((i) => i.id === cartItem.id);
-          return <CartItem key={cartItem.id} cartItem={item} />;
-        })}
-
-
-
+          {cartItems.map((cartItem) => {
+            const item = fetchedCartItems.find((i) => i.id === cartItem.id);
+            if (!item) return null;
+            return (
+              <CartItem
+                key={cartItem.id}
+                id={cartItem.id}
+                quantity={cartItem.quantity}
+              />
+            );
+          })}
           <div className="ms-auto fw-bold fs-5">
             Total{" "}
             {formatCurrency(
               cartItems.reduce((total, cartItem) => {
-                const foundItem = fetchedCartItems.find(
-                  (i) => i.id === cartItem.id
-                );
-                return total + (foundItem?.price || 0) * cartItem.quantity;
+                const item = fetchedCartItems.find((i) => i.id === cartItem.id);
+                return total + (item?.price || 0) * cartItem.quantity;
               }, 0)
             )}
           </div>
